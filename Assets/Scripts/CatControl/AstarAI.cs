54fabef5435cd749ed8b98f45ee7d1759a2cf1da
张兆鳌 <captain.cook.83@gmail.com>
2017-04-06 17:47:35 +0800

    修改角色跳跃控制器逻辑
    跳跃控制过程中的位移全部采用控制器逻辑实现，不再依靠动画状态机的脚本；增加跳跃动画的三个事件回调，用以与跳跃控制器进行同步；

---
+++ Assets/Scripts/CatControl/AstarAI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Pathfinding;
+
+public class AstarAI : MonoBehaviour {
+	// The point to move to
+	public Transform targetPosition;
+	private Seeker seeker;
+	// The calculated path
+	public Path path;
+	// The AI's speed in meters per second
+	public float speed = 2;
+	// The max distance from the AI to a waypoint for it to continue to the next waypoint
+	public float nextWaypointDistance = 3;
+	// The waypoint we are currently moving towards
+	private int currentWaypoint = 0;
+	// How often to recalculate the path (in seconds)
+	public float repathRate = 0.5f;
+	private float lastRepath = -9999;
+
+	void Start () {
+		seeker = GetComponent<Seeker>();
+		seeker.StartPath(transform.position, targetPosition.position, OnPathComplete);
+	}
+
+	void OnPathComplete (Path p) {
+		Debug.Log("A path was calculated. Did it fail with an error? " + p.error);
+		if (!p.error) {
+			path = p;
+			// Reset the waypoint counter so that we start to move towards the first point in the path
+			currentWaypoint = 0;
+		}
+	}
+
+	void Update () {
+		if (path == null) {
+			// We have no path to follow yet, so don't do anything
+			return;
+		}
+		if (currentWaypoint > path.vectorPath.Count) return;
+		if (currentWaypoint == path.vectorPath.Count) {
+			Debug.Log("End Of Path Reached");
+			currentWaypoint++;
+			return;
+		}
+		// Direction to the next waypoint
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
+		transform.Translate(dir * speed * Time.deltaTime);
+		// The commented line is equivalent to the one below, but the one that is used
+		// is slightly faster since it does not have to calculate a square root
+		//if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
+		if ((transform.position-path.vectorPath[currentWaypoint]).sqrMagnitude < nextWaypointDistance*nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
+	}
+} 
---
+++ Assets/Scripts/CatControl/JumpStateController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpStateController : MonoBehaviour {
+
+	public Transform jumpTarget;
+
+	public string rootMotionParameterName = "MovementSpeed";
+
+	public float timedUpwardSpeed = 10F;
+
+	public float minVerticalJumpHeight = 0.1F;
+	public float stopOffset = 0.1F;
+	public float normalizedForwardDistance = 0.24F;
+
+	private Rigidbody rigidbody;
+	private Animator animator;
+
+	private Vector3 targetPosition = Vector3.zero;
+	private Vector3 targetDirection = Vector3.zero;
+
+	private float multipleForwardSpeed = 1.0F;
+	private bool isJumpUpward = false;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	void OnAnimatorMove () {
+		if (targetPosition != Vector3.zero) {
+			Vector3 position = transform.position;
+			bool moveEnd = false;
+
+			if (isJumpUpward) {
+				position = Vector3.Lerp (position, targetPosition, Time.deltaTime * timedUpwardSpeed);
+				moveEnd = position.y > targetPosition.y;
+			} else {
+				position.z += animator.GetFloat(rootMotionParameterName) * Time.deltaTime * multipleForwardSpeed;
+				if (targetDirection.y <= - minVerticalJumpHeight) {
+					moveEnd = position.y <= targetPosition.y;
+				}
+			}
+
+			transform.position = position;
+			if (moveEnd || Vector3.Distance (position, targetPosition) < stopOffset) {		// 可以考虑去除的判断
+				OnJumpEndEvent ();
+				OnJumpStopEvent ();
+			}
+		}
+	}
+
+	void OnJumpStartEvent() {
+		rigidbody.isKinematic = true;
+
+		if (jumpTarget.gameObject.activeSelf) {
+			targetPosition = jumpTarget.position;
+		} else {
+			targetPosition = transform.position + transform.forward * normalizedForwardDistance;
+		}
+
+		targetDirection = targetPosition - transform.position;
+		isJumpUpward = targetDirection.y >= minVerticalJumpHeight;
+
+		if (isJumpUpward) {
+			multipleForwardSpeed = 1.0F;
+			targetPosition += new Vector3(0, 0.1F, 0);
+		} else {
+			multipleForwardSpeed = Mathf.Max (1.0F, Mathf.Abs(targetDirection.z) / normalizedForwardDistance);
+		}
+	}
+
+	void OnJumpEndEvent() {
+		rigidbody.isKinematic = false;
+	}
+
+	void OnJumpStopEvent() {
+		targetPosition = Vector3.zero;
+	}
+}
