6e9e4763f10688202b860e1578f89056623193c6
张兆鳌 <captain.cook.83@gmail.com>
2017-04-22 16:57:31 +0800

    启动时加载AStar导航数据文件，避免运行时scan导致的开销

---
+++ Assets/Scripts/AStarNavmeshLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarNavmeshLoader : MonoBehaviour {
+
+	public TextAsset graphData;
+
+	void Start () {
+		AstarPath.active.astarData.DeserializeGraphs (graphData.bytes);
+	}
+}
